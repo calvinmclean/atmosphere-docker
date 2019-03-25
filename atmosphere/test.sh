89b57eff15e8ba86943127525f51fe53e1aa16cb
Calvin McLean <calvinmclean@email.arizona.edu>
2019-03-25 10:54:47 -0700

    Cleanup echo-ing and running commands

--- atmosphere/test.sh
+++ atmosphere/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function echo_and_run() {
+  echo "RUNNING COMMAND: $1"
+  bash -c "$1"
+}
+
 git clone https://github.com/cyverse/atmosphere.git /opt/dev/atmosphere
 cd /opt/dev/atmosphere
 
@@ -16,37 +21,20 @@ pip install pip-tools==1.11.0
 psql -c "CREATE USER atmosphere_db_user WITH PASSWORD 'atmosphere_db_pass' CREATEDB;" -U postgres -h postgres
 psql -c "CREATE DATABASE atmosphere_db WITH OWNER atmosphere_db_user;" -U postgres -h postgres
 
-echo "RUNNING COMMAND: ./travis/check_properly_generated_requirements.sh"
-./travis/check_properly_generated_requirements.sh
-echo "RUNNING COMMAND: pip-sync requirements.txt"
-pip-sync requirements.txt
-echo "RUNNING COMMAND: sed -i "s/DATABASE_HOST = localhost/DATABASE_HOST = postgres/" variables.ini.dist"
-sed -i "s/DATABASE_HOST = localhost/DATABASE_HOST = postgres/" variables.ini.dist
-echo "RUNNING COMMAND: cp ./variables.ini.dist ./variables.ini"
-cp ./variables.ini.dist ./variables.ini
-echo "RUNNING COMMAND: ./configure"
-./configure
-echo "RUNNING COMMAND: python manage.py check"
-python manage.py check
-echo "RUNNING COMMAND: python manage.py makemigrations --dry-run --check"
-python manage.py makemigrations --dry-run --check
-echo "RUNNING COMMAND: patch variables.ini variables_for_testing_cyverse.ini.patch"
-patch variables.ini variables_for_testing_cyverse.ini.patch
-echo "RUNNING COMMAND: ./configure"
-./configure
-echo "RUNNING COMMAND: pip-sync dev_requirements.txt"
-pip-sync dev_requirements.txt
-echo "RUNNING COMMAND: ./travis/check_for_dead_code_with_vulture.sh"
-./travis/check_for_dead_code_with_vulture.sh
-echo "RUNNING COMMAND: yapf --diff -p -- $(git ls-files | grep '\.py$')"
-yapf --diff -p -- $(git ls-files | grep '\.py$')
-echo "RUNNING COMMAND: prospector --profile prospector_profile.yaml --messages-only -- $(git ls-files | grep '\.py$')"
-prospector --profile prospector_profile.yaml --messages-only -- $(git ls-files | grep '\.py$')
-echo "RUNNING COMMAND: python manage.py test --keepdb --noinput --settings=atmosphere.settings"
-python manage.py test --keepdb --noinput --settings=atmosphere.settings
-echo "RUNNING COMMAND: python manage.py behave --keepdb --tags ~@skip-if-cyverse --settings=atmosphere.settings --format rerun --outfile rerun_failing.features"
-python manage.py behave --keepdb --tags ~@skip-if-cyverse --settings=atmosphere.settings --format rerun --outfile rerun_failing.features
-echo "RUNNING COMMAND: if [ -f "rerun_failing.features" ]; then python manage.py behave --logging-level DEBUG --capture-stderr --capture --verbosity 3 --keepdb @rerun_failing.features; fi"
-if [ -f "rerun_failing.features" ]; then python manage.py behave --logging-level DEBUG --capture-stderr --capture --verbosity 3 --keepdb @rerun_failing.features; fi
-echo "RUNNING COMMAND: python manage.py makemigrations --dry-run --check"
-python manage.py makemigrations --dry-run --check
+echo_and_run "./travis/check_properly_generated_requirements.sh"
+echo_and_run "pip-sync requirements.txt"
+echo_and_run "sed -i 's/DATABASE_HOST = localhost/DATABASE_HOST = postgres/' variables.ini.dist"
+echo_and_run "cp ./variables.ini.dist ./variables.ini"
+echo_and_run "./configure"
+echo_and_run "python manage.py check"
+echo_and_run "python manage.py makemigrations --dry-run --check"
+echo_and_run "patch variables.ini variables_for_testing_cyverse.ini.patch"
+echo_and_run "./configure"
+echo_and_run "pip-sync dev_requirements.txt"
+echo_and_run "./travis/check_for_dead_code_with_vulture.sh"
+echo_and_run "yapf --diff -p -- $(git ls-files | grep '\.py$')"
+echo_and_run "prospector --profile prospector_profile.yaml --messages-only -- $(git ls-files | grep '\.py$')"
+echo_and_run "python manage.py test --keepdb --noinput --settings=atmosphere.settings"
+echo_and_run "python manage.py behave --keepdb --tags ~@skip-if-cyverse --settings=atmosphere.settings --format rerun --outfile rerun_failing.features"
+echo_and_run "if [ -f 'rerun_failing.features' ]; then python manage.py behave --logging-level DEBUG --capture-stderr --capture --verbosity 3 --keepdb @rerun_failing.features; fi"
+echo_and_run "python manage.py makemigrations --dry-run --check"
